7d3c6d5a17d85b6fdb544f4fb8057ea3e9cce122
Justin Kunimune <justinkunimune@gmail.com>
2016-04-01 11:16:57 -0400

    All the Data
    I have completed the data compilation portion of my code. Now all that
    remains is to produce the image.

--- src/timeline/TimelineGenerator.java
+++ src/timeline/TimelineGenerator.java
@@ -13,10 +13,26 @@ public class TimelineGenerator {
 		BufferedReader empireReader = new BufferedReader(new FileReader("Empires.txt"));
 		String line = "";
 		do {
+			if (line.isEmpty())
+				empires.add(new Empire(Integer.parseInt(line.substring(0,1)), line.substring(1)));
 			line = empireReader.readLine();
-			empires.add(new Empire(Integer.parseInt(line.substring(0,1)), line.substring(1)));
 		} while (line != null);
 		empireReader.close();
+		
+		BufferedReader areaReader = new BufferedReader(new FileReader("Timeline.txt"));
+		do {
+			if (line.isEmpty()) {
+				String name = line.substring(10);
+				Empire e;
+				for (int i = 0; i < empires.size(); i ++) {
+					e = empires.get(i);
+					if (e.name().equals(name))
+						break;
+				}
+				e.addPoint(Integer.parseInt(line.substring(0,5)), Integer.parseInt(line.substring(6,9)));
+			}
+			line = empireReader.readLine();
+		} while (line != null);
 	}
 
 }
